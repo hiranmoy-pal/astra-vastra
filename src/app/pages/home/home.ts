5239631cf6c0731399a6f3f9be67a45113e182b5
Hiranmoy_Pal <palhiranmoy1999@gmail.com>
2026-04-06 01:43:58 +0530

    home hero and banner

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, Inject, OnInit, PLATFORM_ID, signal, ViewChild } from '@angular/core';
 import { Header } from '../../layout/header/header';
 import { Footer } from '../../layout/footer/footer';
 import { Theme } from '../../core/services/theme/theme';
@@ -9,17 +9,60 @@ import { Loading } from '../../core/services/loading/loading';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header, Footer],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [Header, Footer, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 
-export class Home implements OnInit {
+export class Home implements OnInit, AfterViewInit {
+
+  @ViewChild('swiperRefBanner') swiperRefBanner!: ElementRef;
+  banners = [
+    'https://rukminim2.flixcart.com/fk-p-flap/3160/1540/image/d17697e94e3af1ff.jpg?q=60',
+    'https://rukminim2.flixcart.com/fk-p-flap/3160/1540/image/5017a43fc014e1f6.png?q=60',
+    'https://rukminim2.flixcart.com/fk-p-flap/3160/1540/image/c966eebea8d80fcb.png?q=60',
+    'https://rukminim2.flixcart.com/fk-p-flap/3160/1540/image/4e25a99384af1be3.png?q=60',
+    'https://rukminim2.flixcart.com/fk-p-flap/3160/1540/image/428ea77977271cc4.png?q=60'
+  ];
+
+  bannerSwiperConfig = signal({
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      clickable: true
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      576: {
+        slidesPerView: 1.2
+      },
+      768: {
+        slidesPerView: 1.2
+      },
+      1024: {
+        slidesPerView: 1.3
+      },
+      1200: {
+        slidesPerView: 1.7
+      },
+      1280: {
+        slidesPerView: 1.7
+      }
+    }
+  });
 
   constructor(
     public themeService: Theme,
     public toastService: Toast,
-    public loadingService: Loading
+    public loadingService: Loading,
+    @Inject(PLATFORM_ID) private platformId: Object,
   ) { }
 
   ngOnInit() {
@@ -28,5 +71,13 @@ export class Home implements OnInit {
     setTimeout(() => {
       this.loadingService.hide();
     }, 2000);
+  }
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        Object.assign(this.swiperRefBanner.nativeElement, this.bannerSwiperConfig());
+        this.swiperRefBanner.nativeElement.initialize();
+      }, 400);
+    }
   }
 }
