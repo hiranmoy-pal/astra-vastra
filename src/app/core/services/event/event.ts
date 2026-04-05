0fa82d633dab13ea7e1bfc6e7c92a72057a2d17e
Hiranmoy_Pal <palhiranmoy1999@gmail.com>
2026-04-05 14:02:17 +0530

    header designe completed and add event service

--- src/app/core/services/event/event.ts
+++ src/app/core/services/event/event.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, signal } from '@angular/core';
+type EventMap = Record<string, any>;
 @Injectable({
   providedIn: 'root',
 })
-export class Event {}
+export class Event {
+
+  private events = signal<EventMap>({});
+
+  emit<T>(key: string, data?: T) {
+    this.events.update(state => ({
+      ...state,
+      [key]: data
+    }));
+  }
+
+  on<T>(key: string) {
+    return () => this.events()[key] as T;
+  }
+
+  clear(key: string) {
+    this.events.update(state => {
+      const newState = { ...state };
+      delete newState[key];
+      return newState;
+    });
+  }
+}
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Theme } from '../../core/services/theme/theme';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,8 @@ import { Component } from '@angular/core';
 })
 export class Header {
  menu: string | null = null;
+
+ constructor(public themeService: Theme){}
 
 setMenu(menu: string) {
   this.menu = menu;
