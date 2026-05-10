56d8f40321add4133b7f273b7831ac00a29a84d6
Hiranmoy_Pal <palhiranmoy1999@gmail.com>
2026-05-10 21:38:20 +0530

    product-list and details fixed

--- src/app/pages/product-list/product-list.ts
+++ src/app/pages/product-list/product-list.ts
@@ -1,18 +1,462 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { Header } from '../../layout/header/header';
 import { Footer } from '../../layout/footer/footer';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 import { ProfileMenu } from '../../layout/profile-menu/profile-menu';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
- standalone: true,
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
-    Header, Footer, CommonModule, RouterModule, RouterLink, ProfileMenu, ReactiveFormsModule
+    Header, Footer, CommonModule, RouterModule, RouterLink, ProfileMenu, FormsModule, ReactiveFormsModule
   ],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
-export class ProductList {}
+export class ProductList implements OnInit, AfterViewInit {
+  hoverTimers = new WeakMap<any, any>();
+  selectedDiscount = 25;
+  minPrice = 250;
+  maxPrice = 1200;
+
+  categories = [
+    { name: 'Evening Wear', count: 142, selected: false },
+    { name: 'Knitwear', count: 86, selected: true },
+    { name: 'Outerwear', count: 210, selected: false },
+  ];
+
+  brands = [
+    { name: 'Atelier Essentials', count: 412, selected: false },
+    { name: 'Modern Heritage', count: 195, selected: false },
+    { name: 'Signature Line', count: 284, selected: false },
+  ];
+
+  genders = [
+    { name: 'Women', count: 845, selected: true },
+    { name: 'Men', count: 403, selected: false },
+    { name: 'Unisex', count: 156, selected: false },
+  ];
+
+  colors = [
+    { name: 'Midnight', code: '#111111', count: 212, selected: false },
+    { name: 'Burgundy', code: '#720036', count: 89, selected: true },
+    { name: 'Oatmeal', code: '#E5D3B3', count: 145, selected: false },
+    { name: 'Ivory', code: '#F8F5EE', count: 98, selected: false },
+  ];
+
+  sizes = [
+    { name: 'XS', selected: false },
+    { name: 'S', selected: true },
+    { name: 'M', selected: true },
+    { name: 'L', selected: false },
+    { name: 'XL', selected: false },
+  ];
+
+  ratings = [
+    { name: '4+ Stars', selected: false },
+    { name: '3+ Stars', selected: false },
+  ];
+
+  discounts = [
+    { label: '10% Off or more', value: 10 },
+    { label: '25% Off or more', value: 25 },
+    { label: '50% Off or more', value: 50 },
+  ];
+
+  products = [
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit Round Neck Leg Suit Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/p/d/9/free-bebydoll-night-dress-unitrust-original-imah6t58na43hebf.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/g/1/l/free-bebydoll-night-dress-unitrust-original-imah6t58hqkamhjh.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/babydoll/n/o/l/free-s-9-red-fs-sgc-sweden-original-imahytcv8etjtcwh.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/u/b/5/free-bebydoll-night-dress-unitrust-original-imah6t58h9mguknu.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/i/n/p/free-bebydoll-night-dress-unitrust-original-imahh7jhrwskbbdr.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/u/l/r/free-bebydoll-night-dress-unitrust-original-imahh7jh6au9rhpp.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/r/q/c/free-bebydoll-night-dress-unitrust-original-imahh7jhdzbsfkqj.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/e/c/w/48-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczdqzyf7zh.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/c/q/f/46b-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczwcqxw9zs.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/o/b/e/48-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczvpsycrt5.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/y/g/x/48-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczvbzzbvvf.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/8/v/i/48-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczznytwew7.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/e/r/4/30-18mm-non-padded-set-oumar-bibi-original-imahbhhx7yvbshup.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/j/z/e/34-18mm-nonpadded-oumar-bibi-original-imahbhh3gf2nzsma.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shopsy-lingerie-set/a/e/o/40-18mmnonpad-oumar-bibi-original-imahb3ty6gv3ezkv.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/j/x/m/34-v4nbrs-maroon-34-pack-1-vanila-original-imahkyjmzs9sbxs6.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/r/y/7/34-v4nbrs-maroon-34-pack-1-vanila-original-imahkyjmavkrbb5v.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/a/v/4/34-v4nbrs-maroon-34-pack-1-vanila-original-imahkyjmfyppzts7.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }, {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }
+  ];
+
+  toggleSize(size: any) {
+    size.selected = !size.selected;
+  }
+
+  ngOnInit(): void {
+
+  }
+  ngAfterViewInit(): void {
+
+  }
+
+  startSwiper(event: any) {
+    const container = event.currentTarget;
+    const swiperEl = container.querySelector('swiper-container');
+
+    if (!swiperEl) return;
+
+    if (!swiperEl.classList.contains('swiper-initialized')) {
+      Object.assign(swiperEl, {
+        loop: true,
+        speed: 500,
+        autoplay: {
+          delay: 600,
+          disableOnInteraction: false
+        },
+        pagination: {
+          clickable: true
+        }
+      });
+
+      setTimeout(() => { swiperEl.initialize(); }, 400);
+    }
+
+    const swiper = swiperEl.swiper;
+    if (!swiper) return;
+
+    swiper.autoplay.stop();
+    swiper.slideToLoop(0, 0);
+
+    if (this.hoverTimers.has(container)) {
+      clearTimeout(this.hoverTimers.get(container));
+    }
+
+    const timer = setTimeout(() => {
+      container.classList.add('swiper-active');
+
+      swiper.autoplay.start();
+    }, 600);
+
+    this.hoverTimers.set(container, timer);
+  }
+
+  resetSwiper(event: any) {
+    const container = event.currentTarget;
+    const swiperEl = container.querySelector('swiper-container');
+    const swiper = swiperEl?.swiper;
+
+    if (this.hoverTimers.has(container)) {
+      clearTimeout(this.hoverTimers.get(container));
+      this.hoverTimers.delete(container);
+    }
+
+    container.classList.remove('swiper-active');
+
+    if (swiper) {
+      swiper.autoplay.stop();
+      swiper.slideToLoop(0, 0);
+    }
+  }
+}
