970a50f21c8605b2f2b8ef801ed090927043c7d1
Hiranmoy_Pal <palhiranmoy1999@gmail.com>
2026-05-14 19:22:43 +0530

    designe fixing

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -1,18 +1,288 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, Inject, OnInit, PLATFORM_ID, signal, ViewChild } from '@angular/core';
 import { Header } from '../../layout/header/header';
 import { Footer } from '../../layout/footer/footer';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 import { ProfileMenu } from '../../layout/profile-menu/profile-menu';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Theme } from '../../core/services/theme/theme';
+import { Toast } from '../../core/services/toast/toast';
+import { Loading } from '../../core/services/loading/loading';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     Header, Footer, CommonModule, RouterModule, RouterLink, ProfileMenu, ReactiveFormsModule
   ],
   templateUrl: './cart.html',
   styleUrl: './cart.scss',
 })
-export class Cart {}
+export class Cart implements OnInit, AfterViewInit {
+  hoverTimers = new WeakMap<any, any>();
+  @ViewChild('swiperSaveForLater') swiperSaveForLater!: ElementRef;
+
+  saveForLaterSwiperConfig = signal({
+    navigation: false,
+    pagination: false,
+    breakpoints: {
+      0: {
+        slidesPerView: 1.2,
+        spaceBetween: 12
+      },
+
+      768: {
+        slidesPerView: 3.2,
+        spaceBetween: 16
+      },
+
+      1024: {
+        slidesPerView: 4.2,
+        spaceBetween: 20
+      },
+
+      1400: {
+        slidesPerView: 4.4,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  products = [
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit Round Neck Leg Suit Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/p/d/9/free-bebydoll-night-dress-unitrust-original-imah6t58na43hebf.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/g/1/l/free-bebydoll-night-dress-unitrust-original-imah6t58hqkamhjh.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/babydoll/n/o/l/free-s-9-red-fs-sgc-sweden-original-imahytcv8etjtcwh.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/u/b/5/free-bebydoll-night-dress-unitrust-original-imah6t58h9mguknu.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/i/n/p/free-bebydoll-night-dress-unitrust-original-imahh7jhrwskbbdr.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/u/l/r/free-bebydoll-night-dress-unitrust-original-imahh7jh6au9rhpp.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/r/q/c/free-bebydoll-night-dress-unitrust-original-imahh7jhdzbsfkqj.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/e/c/w/48-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczdqzyf7zh.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/c/q/f/46b-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczwcqxw9zs.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/o/b/e/48-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczvpsycrt5.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/y/g/x/48-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczvbzzbvvf.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/8/v/i/48-bf-farha-broadbelt-set-rani-body-figure-original-imahhzczznytwew7.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/e/r/4/30-18mm-non-padded-set-oumar-bibi-original-imahbhhx7yvbshup.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/j/z/e/34-18mm-nonpadded-oumar-bibi-original-imahbhh3gf2nzsma.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shopsy-lingerie-set/a/e/o/40-18mmnonpad-oumar-bibi-original-imahb3ty6gv3ezkv.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/j/x/m/34-v4nbrs-maroon-34-pack-1-vanila-original-imahkyjmzs9sbxs6.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/r/y/7/34-v4nbrs-maroon-34-pack-1-vanila-original-imahkyjmavkrbb5v.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/lingerie-set/a/v/4/34-v4nbrs-maroon-34-pack-1-vanila-original-imahkyjmfyppzts7.jpeg?q=90'
+      ]
+    },
+    {
+      brand: 'Keepfit',
+      name: 'Round Neck Leg Suit',
+      price: 1398,
+      original: 3499,
+      discount: '(60% OFF)',
+      size: 'S, M, L',
+      rating: 4.7,
+      count: '31',
+      images: [
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/5/2/q/-original-imahgcs8chzjtsmz.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/k/c/6/-original-imahjhjpv7txxxsu.jpeg?q=90',
+        'https://rukminim1.flixcart.com/image/1536/1536/xif0q/shoe/h/m/l/-original-imahjhjpzftwmgbb.jpeg?q=90'
+      ]
+    }
+  ];
+
+  constructor(
+    public themeService: Theme,
+    public toastService: Toast,
+    public loadingService: Loading,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        Object.assign(this.swiperSaveForLater.nativeElement, this.saveForLaterSwiperConfig());
+        this.swiperSaveForLater.nativeElement.initialize();
+      }, 400);
+    }
+  }
+
+  startSwiper(event: any) {
+    const container = event.currentTarget;
+    const swiperEl = container.querySelector('swiper-container');
+
+    if (!swiperEl) return;
+
+    if (!swiperEl.classList.contains('swiper-initialized')) {
+      Object.assign(swiperEl, {
+        loop: true,
+        speed: 500,
+        autoplay: {
+          delay: 600,
+          disableOnInteraction: false
+        },
+        pagination: {
+          clickable: true
+        }
+      });
+
+      setTimeout(() => { swiperEl.initialize(); }, 400);
+    }
+
+    const swiper = swiperEl.swiper;
+    if (!swiper) return;
+
+    swiper.autoplay.stop();
+    swiper.slideToLoop(0, 0);
+
+    if (this.hoverTimers.has(container)) {
+      clearTimeout(this.hoverTimers.get(container));
+    }
+
+    const timer = setTimeout(() => {
+      container.classList.add('swiper-active');
+
+      swiper.autoplay.start();
+    }, 600);
+
+    this.hoverTimers.set(container, timer);
+  }
+
+  resetSwiper(event: any) {
+    const container = event.currentTarget;
+    const swiperEl = container.querySelector('swiper-container');
+    const swiper = swiperEl?.swiper;
+
+    if (this.hoverTimers.has(container)) {
+      clearTimeout(this.hoverTimers.get(container));
+      this.hoverTimers.delete(container);
+    }
+
+    container.classList.remove('swiper-active');
+
+    if (swiper) {
+      swiper.autoplay.stop();
+      swiper.slideToLoop(0, 0);
+    }
+  }
+}
